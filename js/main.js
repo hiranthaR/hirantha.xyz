8109199e1e49642de43f9f726a529b541aa82ef1
hirantha <sahanhirantha@gmail.com>
2020-01-26 13:20:02 +0530

    recreated view to terminal

--- js/main.js
+++ js/main.js
@@ -1,206 +1,63 @@
- AOS.init({
- 	duration: 800,
- 	easing: 'slide'
- });
+var isPausePrompt = true;
+var promptId = 0;
 
-$(document).ready(function($) {
+// function promptUnderLine() {
+//     if (isPausePrompt) return;
+//     var consoleDisplay = document.getElementById("console-display");
+//     if (consoleDisplay.innerText.slice(-1) === "_") {
+//         consoleDisplay.innerHTML = consoleDisplay.innerText.substring(0, consoleDisplay.innerText.length - 2);
+//     } else {
+//         consoleDisplay.innerHTML = consoleDisplay.innerText + " _";
+//     }
+// }
 
-	"use strict";
+// setInterval(promptUnderLine, 700);
 
-	$(window).stellar({
-    responsive: false,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
+// function pausePrompt() {
+//     isPausePrompt = !isPausePrompt;
+//     var consoleDisplay = document.getElementById("console-display");
+//     if (consoleDisplay.innerText.slice(-1) === "_") {
+//         consoleDisplay.innerHTML = consoleDisplay.innerText.substring(0, consoleDisplay.innerText.length - 2);
+//     }
+// }
 
-	// Scrollax
-  $.Scrollax();
+addEventListenerForConsolePrompt()
+setFocusToPrompt()
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var n = new Date();
+var y = n.getFullYear();
+var m = n.getMonth() + 1;
+var d = n.getDate();
+var day = n.getDay();
+document.getElementById("date").innerHTML = days[day] + " " + d + " " + months[m] + " " + y;
 
+function handlePrompt() {
+    var consoleDisplay = document.getElementById("console-display");
+    var consolePrompt = document.getElementById("console-prompt");
+    var consolePromptLine = document.getElementById("console-prompt-line-" + promptId);
+    var displayHtml = consoleDisplay.innerHTML;
+    var oldPromptLine = consolePromptLine.innerHTML + consolePrompt.value;
+    displayHtml = displayHtml.replace(consolePromptLine.innerHTML, oldPromptLine);
+    displayHtml = displayHtml.replace(consolePrompt.outerHTML, "");
+    consoleDisplay.innerHTML = displayHtml;
+    consoleDisplay.innerHTML += "sex";
+    var newPromptLine = consolePromptLine.outerHTML.replace("console-prompt-line-" + promptId, "console-prompt-line-" + ++promptId);
+    consoleDisplay.innerHTML += newPromptLine;
+    addEventListenerForConsolePrompt();
+    setFocusToPrompt();
+}
 
-	// loader
-	var loader = function() {
-		setTimeout(function() { 
-			if($('#ftco-loader').length > 0) {
-				$('#ftco-loader').removeClass('show');
-			}
-		}, 1);
-	};
-	loader();
+function setFocusToPrompt() {
+    document.getElementById("console-prompt").focus();
+}
 
-	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    dots: false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1,
-	        nav:false
-	      },
-	      600:{
-	        items:1,
-	        nav:false
-	      },
-	      1000:{
-	        items:1,
-	        nav:false
-	      }
-	    }
-	   });
-	};
-	carousel();
+function addEventListenerForConsolePrompt() {
+    document.getElementById("console-prompt").addEventListener("keypress", function (event) {
+        if (event.keyCode === 13) {
+            handlePrompt()
+        }
+    });
+}
 
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	var burgerMenu = function() {
-
-		$('.js-colorlib-nav-toggle').on('click', function(event) {
-			event.preventDefault();
-			var $this = $(this);
-			if( $('body').hasClass('menu-show') ) {
-				$('body').removeClass('menu-show');
-				$('#colorlib-main-nav > .js-colorlib-nav-toggle').removeClass('show');
-			} else {
-				$('body').addClass('menu-show');
-				setTimeout(function(){
-					$('#colorlib-main-nav > .js-colorlib-nav-toggle').addClass('show');
-				}, 900);
-			}
-		})
-	};
-	burgerMenu();
-	
-	var counter = function() {
-		
-		$('#section-counter').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number').each(function(){
-					var $this = $(this),
-						num = $this.data('number');
-						console.log(num);
-					$this.animateNumber(
-					  {
-					    number: num,
-					    numberStep: comma_separator_number_step
-					  }, 7000
-					);
-				});
-				
-			}
-
-		} , { offset: '95%' } );
-
-	}
-	counter();
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.ftco-animate').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .ftco-animate.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn ftco-animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft ftco-animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight ftco-animated');
-							} else {
-								el.addClass('fadeInUp ftco-animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 50, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '95%' } );
-	};
-	contentWayPoint();
-
-
-	// magnific popup
-	$('.image-popup').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: false,
-    fixedContentPos: true,
-    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-     gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      verticalFit: true
-    },
-    zoom: {
-      enabled: true,
-      duration: 300 // don't foget to change the duration also in CSS
-    }
-  });
-
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-
-    fixedContentPos: false
-  });
-
-   
-   $('#appointment_date').datepicker({
-	  'format': 'm/d/yyyy',
-	  'autoclose': true
-	});
-	$('#appointment_time').timepicker();
-
-	var pageProgress = function() {
-		$(window).scroll(function() {
-	    var wintop = $(window).scrollTop(), docheight = $('.page').height(), winheight = $(window).height();
-	    // console.log(wintop);
-	    var totalScroll = (wintop/(docheight-winheight))*100;
-	    // console.log("total scroll" + totalScroll);
-	    $(".KW_progressBar").css("width",totalScroll+"%");
-	  });
-
-	};
-	pageProgress();
-
-
-});
-
+showDate()
